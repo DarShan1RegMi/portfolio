dca908695f4db6ce2c1545fe50087d5f37b5cb63
Darshan Regmi <105899428+DarShan1RegMi@users.noreply.github.com>
2024-06-26 18:42:06 +0545

    Add files via upload

---
+++ docs/src/components/contact.js
@@ -0,0 +1,13 @@
+// Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1>Get in touch</h1>
+      <p>Contact me at <a href="mailto:your_email@example.com">your_email@example.com</a>.</p>
+    </div>
+  );
+}
+
+export default Contact;
